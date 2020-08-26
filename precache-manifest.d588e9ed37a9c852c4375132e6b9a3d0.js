c1e59c5e193c10b108e318088d34418d80f6386b
Jakub Jureczko <untech.untech@gmail.com>
2020-08-26 14:31:59 +0200

    Updates

---
+++ precache-manifest.d588e9ed37a9c852c4375132e6b9a3d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a387363272742bc1ea63dfe18ddffa0",
+    "url": "/contentful-blog-project/index.html"
+  },
+  {
+    "revision": "f33750a2b7b62a634f1d",
+    "url": "/contentful-blog-project/static/css/main.341b574e.chunk.css"
+  },
+  {
+    "revision": "f4d10fff4d23e027f70c",
+    "url": "/contentful-blog-project/static/js/2.0140f6c3.chunk.js"
+  },
+  {
+    "revision": "de21e3d16a11d6eec3f0c3d6c6256755",
+    "url": "/contentful-blog-project/static/js/2.0140f6c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33750a2b7b62a634f1d",
+    "url": "/contentful-blog-project/static/js/main.4861cefa.chunk.js"
+  },
+  {
+    "revision": "9fe64d86f490aeb0ab85",
+    "url": "/contentful-blog-project/static/js/runtime-main.81213151.js"
+  },
+  {
+    "revision": "9fd8b6a0fbee467626d818fc69dbe813",
+    "url": "/contentful-blog-project/static/media/505.9fd8b6a0.jpg"
+  }
+]);
